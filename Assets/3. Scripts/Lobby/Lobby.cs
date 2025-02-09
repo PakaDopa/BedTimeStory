31dbcf94bd3a0d56c228db01f8907f1ecd893858
김중현 <110381888+gotkagovkfl@users.noreply.github.com>
2025-02-09 14:17:35 +0900

    [기능추가] 가이드 기능 제작

--- Assets/3. Scripts/Lobby/Lobby.cs
+++ Assets/3. Scripts/Lobby/Lobby.cs
@@ -5,9 +5,16 @@ using UnityEngine.UI;
 
 public class Lobby : DestroyableSingleton<Lobby>
 {
+    [Header("Dashboard")]
     [SerializeField] Button btn_dashboard;
     [SerializeField] DashboardPanel dashboardPanel;
     
+
+    [Header("Guide")]
+    [SerializeField] Button btn_guide;
+    [SerializeField] GuidePanel guidePanel;
+
+
     
     // Start is called before the first frame update
     void Start()
@@ -22,7 +29,7 @@ public class Lobby : DestroyableSingleton<Lobby>
         GameManager.Instance.PauseGamePlay(false);
     
         btn_dashboard.onClick.AddListener(dashboardPanel.Open);
-    
+        btn_guide.onClick.AddListener(guidePanel.Open);
     
     
     }
